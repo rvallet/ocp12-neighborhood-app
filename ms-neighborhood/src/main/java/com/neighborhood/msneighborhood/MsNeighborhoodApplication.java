c88cd3b5a3c14ff06c9bb024c77b068319bef3d3
rvallet <remy.vallet@gmail.com>
2021-05-14 11:52:05 +0200

    #1 : init project
    cloud-config, ms and website

---
+++ ms-neighborhood/src/main/java/com/neighborhood/msneighborhood/MsNeighborhoodApplication.java
@@ -0,0 +1,13 @@
+package com.neighborhood.msneighborhood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsNeighborhoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsNeighborhoodApplication.class, args);
+    }
+
+}
